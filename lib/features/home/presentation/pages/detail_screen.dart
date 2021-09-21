5fa7e6a8e4ed22edef17f3dfab52b04f699b6243
Ringlex <bercik.1122@wp.pl>
2021-09-21 18:25:11 +0200

    Preparing list of tools for given room

--- lib/features/home/presentation/pages/detail_screen.dart
+++ lib/features/home/presentation/pages/detail_screen.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:home_app/features/home/presentation/provider/room_tools.dart';
 
-class Detailscreen extends StatefulWidget {
+class DetailScreen extends StatefulWidget {
   final int id;
-  final List<Tool> toolList; 
+  final List<Tool> toolList;
+  final Icon icon;
+  final String title;
+  final String toolName;
 
-  const Detailscreen({Key key, this.id, this.toolList}) : super(key: key);
+  const DetailScreen(
+      {Key key, this.id, this.toolList, this.icon, this.title, this.toolName})
+      : super(key: key);
 
   @override
-  _DetailscreenState createState() => _DetailscreenState();
+  _DetailScreenState createState() => _DetailScreenState();
 }
 
-class _DetailscreenState extends State<Detailscreen>
+class _DetailScreenState extends State<DetailScreen>
     with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     backgroundColor: Color(0xFF0D1117),
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return Column(
             children: [
-              TopBar(),
-              RoomTools()
+              SizedBox(
+                height: 30,
+              ),
+              TopBar(
+                title: widget.toolName,
+              ),
+              RoomTools(
+                id: widget.id,
+                toolName: widget.toolName,
+                toolRoomList: widget.toolList,
+                title: widget.title,
+              )
             ],
           );
         },
@@ -31,62 +47,174 @@ class _DetailscreenState extends State<Detailscreen>
 }
 
 class TopBar extends StatefulWidget {
+  final double constrains;
+  final String title;
+
+  const TopBar({Key key, this.constrains, this.title}) : super(key: key);
   @override
   _TopBarState createState() => _TopBarState();
 }
 
 class _TopBarState extends State<TopBar> {
-  bool state;
+  bool state = false;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          icon: Icon(Icons.chevron_left),
-          onPressed: () {},
-          iconSize: 30,
-        ),
-        Text('Living room', style: TextStyle(color: Colors.white, fontSize: 26),),
-        Switch(
-          value: state,
-          onChanged: (value) {
-            setState(() {
-              state = value;
-            });
-          },
-          activeColor: Color(0xFF58A6FF),
-          inactiveThumbColor: Colors.grey,
-          inactiveTrackColor: Colors.grey[800],
-        )
-      ],
+    return Container(
+      height: 50,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: [
+          IconButton(
+            icon: Icon(Icons.chevron_left, color: Colors.white, size: 40,),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            iconSize: 30,
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                widget.title,
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
+            ),
+          ),
+          Switch(
+            value: state,
+            onChanged: (value) {
+              setState(() {
+                state = value;
+              });
+            },
+            activeColor: Color(0xFF58A6FF),
+            inactiveThumbColor: Colors.grey,
+            inactiveTrackColor: Colors.grey[800],
+          )
+        ],
+      ),
     );
   }
 }
 
 class RoomTools extends StatefulWidget {
   /// Widget prepared to view a list of tools.
-  /// 
+  ///
   /// After assign a tool it should show a properties of this tool.
- 
 
   final List<Tool> toolRoomList;
   final int id;
+  final Icon icon;
+  final String title;
+  final String toolName;
 
-  const RoomTools({Key key, this.toolRoomList, this.id}) : super(key: key);
+  const RoomTools(
+      {Key key,
+      this.toolRoomList,
+      this.id,
+      this.icon,
+      this.title,
+      this.toolName})
+      : super(key: key);
   @override
   _RoomToolsState createState() => _RoomToolsState();
 }
 
 class _RoomToolsState extends State<RoomTools> {
- 
+  int activeTool = 0;
+  @override
+  void initState() {
+    activeTool = widget.id - 1;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.toolRoomList.length,
-      itemBuilder: (BuildContext context, int index){
-        return Container(); // create some card/container for tool view
-      },
+    return Padding(
+      padding: const EdgeInsets.only(top: 40.0),
+      child: Container(
+        height: 300,
+        width: MediaQuery.of(context).size.width,
+        child: ListView.builder(
+          padding: EdgeInsets.only(left: 10),
+          scrollDirection: Axis.horizontal,
+          itemCount: widget.toolRoomList.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == activeTool) {
+              return GestureDetector(
+                onTap: (){
+                  setState(() {
+                    activeTool = index;
+                  });
+                },
+                              child: Container(
+                  height: 50,
+                  width: 200,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Center(
+                            child: SizedBox(
+                          child: widget.toolRoomList[index].icon,
+                          height: 40,
+                          width: 40,
+                        )),
+                        Center(
+                          child: Text(
+                            widget.toolRoomList[index].title,
+                            style: TextStyle(color: Colors.white, fontSize: 23),
+                          ),
+                        ),
+                        Text(
+                          widget.toolRoomList[index].toolName,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ); // create some card/container for tool view
+            } else {
+              return GestureDetector(
+                onTap: (){
+                  setState(() {
+                    activeTool = index;
+                  });
+                },
+                              child: Container(
+                  height: 50,
+                  width: 200,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Center(
+                            child: SizedBox(
+                          child: widget.toolRoomList[index].icon,
+                          height: 40,
+                          width: 40,
+                        )),
+                        Center(
+                          child: Text(
+                            widget.toolRoomList[index].title,
+                            style: TextStyle(color: Colors.grey, fontSize: 20),
+                          ),
+                        ),
+                        Text(
+                          widget.toolRoomList[index].toolName,
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+            // create some card/container for tool view
+          },
+        ),
+      ),
     );
   }
 }
--- lib/features/home/presentation/widgets/living_room_view.dart
+++ lib/features/home/presentation/widgets/living_room_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_app/features/home/presentation/pages/detail_screen.dart';
 import 'package:home_app/features/home/presentation/provider/room_tools.dart';
 import 'package:home_app/features/home/presentation/widgets/tool_card.dart';
 
@@ -18,10 +19,24 @@ class _LivingRoomViewState extends State<LivingRoomView> {
           child: GridView.count(
             crossAxisCount: 2,
             children: List.generate(lengthList, (index) {
-              return ToolCard(
-                title: livingRoomTools[index].title,
-                toolName: livingRoomTools[index].toolName,
-                icon: livingRoomTools[index].icon,
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailScreen(
+                                id: index + 1,
+                                toolList: livingRoomTools,
+                                icon: livingRoomTools[index].icon,
+                                title: livingRoomTools[index].title,
+                                toolName: livingRoomTools[index].title,
+                              )));
+                },
+                child: ToolCard(
+                  title: livingRoomTools[index].title,
+                  toolName: livingRoomTools[index].toolName,
+                  icon: livingRoomTools[index].icon,
+                ),
               );
             }),
           ),
