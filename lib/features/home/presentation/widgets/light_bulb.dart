33b1c0b844c5e5ae56efde1cf9ea00e58caba73c
Ringlex <bercik.1122@wp.pl>
2021-10-02 21:29:57 +0200

    light bulbe - slider for brightness

--- lib/features/home/presentation/widgets/light_bulb.dart
+++ lib/features/home/presentation/widgets/light_bulb.dart
@@ -6,10 +6,41 @@ class LightBulb extends StatefulWidget {
 }
 
 class _LightBulbState extends State<LightBulb> {
+  double valueBright = 20;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 60),
+              child: Text(
+                'Brightness',
+                style: TextStyle(fontSize: 26, color: Colors.white),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0),
+              child: Slider(
+                activeColor: Color(0xFFF9826C),
+                inactiveColor: Color(0x80F9826C),
+                value: valueBright,
+                onChanged: (double value) {
+                  setState(() {
+                    valueBright = value.roundToDouble();
+                  });
+                },
+                min: 0,
+                max: 100,
+                divisions: 20,
+                label: valueBright.toInt().toString(),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
