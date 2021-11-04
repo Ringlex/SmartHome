a0e64d70c07a2fe143db04110ffe6afdc6c7c602
Ringlex <bercik.1122@wp.pl>
2021-11-04 21:55:40 +0100

    Add tool screen - preapring

---
+++ lib/features/home/presentation/pages/add_tool_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+///Screen to add tool for given room \n
+///
+class AddToolSreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0D1117),
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints contraints) {
+        return Container(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        //TODO: Add possibility to come back from this page -> HomeScreen
+                      },
+                      child: Container(
+                        child: Center(
+                            child: Icon(
+                          Icons.chevron_left,
+                          color: Color(0xFF58A6FF),
+                          size: 30,
+                        )),
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(shape: BoxShape.circle),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Add tool',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                textField('Title'),
+                textField('Name of device'),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget textField(String label) {
+    return Container(child: TextFormField());
+  }
+}
